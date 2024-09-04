62705b609a7162bcca0f5cc00399c44d88410030
Xiang Li <ucaslx@users.noreply.huggingface.co>
2024-09-04 12:11:26 +0000

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def tryon(person_img, garment_img, seed, randomize_seed):
 
     get_start_time =time.time()
     time.sleep(9)
-    Max_Retry = 10
+    Max_Retry = 8
     result_img = None
     info = ""
     err_log = ""
